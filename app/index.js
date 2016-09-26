83c056899db6aed888f49ba1231571e711f3cbc1
Jonathan Apodaca <jonathan.apodaca@colostate.edu>
2016-09-26 15:12:42 -0600

    Scaffolding now works

--- app/index.js
+++ app/index.js
@@ -2,15 +2,34 @@
 
 const co = require('co')
 const generators = require('yeoman-generator')
+const globby = require('globby')
+const path = require('path')
 
 module.exports = generators.Base.extend({
 	constructor: function () {
 		generators.Base.apply(this, arguments)
 	},
 
-	prompting() {
-		return this.prompt([ {
-
+	prompting: co.wrap(function * () {
+		const { name } = yield this.prompt([ {
+			type: 'input',
+			name: 'name',
+			message: 'Your project name',
+			default: this.appname,
 		} ])
-	}
+		this.appname = name
+	}),
+
+	default: co.wrap(function * () {
+		const globs = [ '**/*.*', '**/.*' ].map(p => this.templatePath(p))
+		const files = yield globby(globs)
+		files.forEach(f => {
+			const relativePath = path.relative(this.sourceRoot(), f)
+			this.template(relativePath, relativePath, { appname: this.appname })
+		})
+	}),
+
+	install() {
+		this.npmInstall()
+	},
 })
